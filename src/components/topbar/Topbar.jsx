a13e5c9588e0adfc315776142036e73343b8b21f
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-03-05 23:02:20 +0600

    left side done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -9,8 +9,20 @@ export default function Topbar() {
           <a href="#intro" className="logo">
             genius.
           </a>
+          <div className="itemContainer">
+            <span>++ 019 029 54222</span>
+          </div>
+          <div className="itemContainer">
+            <span>alamin@gmail.com</span>
+          </div>
         </div>
-        <div className="right">right</div>
+        <div className="right">
+          <div className="humburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
