9168ec5dc508fcb11f076ecb2ea20ef120f352b1
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-03-07 12:17:17 +0600

    side navbar done with perfect

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import "./intro.scss";
 
 const Intro = () => {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="imge" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Alamin Shiekh</h1>
+          <h3>
+            Freelance <span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
