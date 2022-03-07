e840f5d312e79f08c56bb35b960855312bf4f698
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-03-07 10:40:50 +0600

    navbar with useState

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./topbar.scss";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar active">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
@@ -17,7 +17,12 @@ export default function Topbar() {
           </div>
         </div>
         <div className="right">
-          <div className="humburger">
+          <div
+            className="humburger"
+            onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}
+          >
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
