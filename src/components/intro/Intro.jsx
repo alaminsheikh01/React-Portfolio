7a7da994cddc4c3465d57b9f0b8d2249c8a14bb6
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-03-07 15:41:42 +0600

    worked on auto typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import "./intro.scss";
+import { init } from "ityped";
 
 const Intro = () => {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -14,7 +25,7 @@ const Intro = () => {
           <h2>Hi There, I'm</h2>
           <h1>Alamin Shiekh</h1>
           <h3>
-            Freelance <span></span>
+            Freelance <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
